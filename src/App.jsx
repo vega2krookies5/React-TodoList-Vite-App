4ab0047440e998b3f0165fd8a87b8dbdb6f202cc
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-24 10:51:44 +0900

    TodoItemList수정 todoList

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ class App extends Component {
         myChange={handleChange}
         myCreate={handleCreate} />
       }>
-        <TodoItemList todoList={todos} />
+        <TodoItemList todosArr={todos} />
       </TodoListTemplate>
     );
   } //render
--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -3,11 +3,14 @@ import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() {
-        const { todos, myToggle, myRemove } = this.props;
+        const { todosArr, myToggle, myRemove } = this.props;
+        const todoList = todosArr.map();
+
         return (
             <div>
-                <TodoItem text="오늘의 할일1" checked={false} />
-                <TodoItem text="오늘의 할일2" checked={true} />
+                {/* <TodoItem text="오늘의 할일1" checked={false} />
+                <TodoItem text="오늘의 할일2" checked={true} /> */}
+                {todoList}
             </div>
         );
     }
