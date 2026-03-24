13818a02e4559606aa48dc16cdfbd9bed4ccecde
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-24 13:35:01 +0900

    TodoItem update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,15 @@ import './TodoItem.css';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+     /*
+        라이프사이클 메서드
+        shouldComponentUpdate(nextProps, nextState) 함수는 렌더링 생략 여부를 결정하는 bool 값을 리턴함
+        true 리턴 (checked 변수에 변동이 있는 경우(같지 않으면))면 render() 함수가 호출됨
+        false 리턴 (checked 변수에 변동이 없는 경우(같으면))면 render() 함수가 호출되지 않음(렌더링 생략)
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.checked !== nextProps.checked;
+    }
     render() {
         const { text, checked, id, onToggle, onRemove } = this.props;
         return (
--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 
 class TodoItemList extends Component {
     /*
-        true 리턴 (myTodos 변수에 변동이 있는 경우)이면 render() 함수가 호출됨
-        false 리턴 (myTodos 변수에 변동이 없는 경우)이면 render() 함수가 호출되지 않음(렌더링 생략)
+        라이프사이클 메서드
+        shouldComponentUpdate(nextProps, nextState) 함수는 렌더링 생략 여부를 결정하는 bool 값을 리턴함
+        true 리턴 (todosArr 변수에 변동이 있는 경우(같지 않으면))면 render() 함수가 호출됨
+        false 리턴 (todosArr 변수에 변동이 없는 경우(같으면))면 render() 함수가 호출되지 않음(렌더링 생략)
     */
     shouldComponentUpdate(nextProps, nextState) {
         return this.props.todosArr !== nextProps.todosArr;
