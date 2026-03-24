392eb4ae42e85c830f6c408a437363a5ba042cee
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-24 13:30:56 +0900

    TodoItemList

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -3,13 +3,21 @@ import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class TodoItemList extends Component {
+    /*
+        true 리턴 (myTodos 변수에 변동이 있는 경우)이면 render() 함수가 호출됨
+        false 리턴 (myTodos 변수에 변동이 없는 경우)이면 render() 함수가 호출되지 않음(렌더링 생략)
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todosArr !== nextProps.todosArr;
+    }
+
     render() {
         const { todosArr, myToggle, myRemove } = this.props;
-        const todoList = todosArr.map(({id,text,checked}) => (
-            <TodoItem 
-                id={id} 
-                text={text} 
-                checked={checked} 
+        const todoList = todosArr.map(({ id, text, checked }) => (
+            <TodoItem
+                id={id}
+                text={text}
+                checked={checked}
                 key={id}
                 onToggle={myToggle}
                 onRemove={myRemove}
